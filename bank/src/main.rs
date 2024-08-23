9459d7e2050cc33d4ab392abf92633ea6d387850
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-08-23 10:23:08 +0200

    Write basic new fns for structs

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -1,11 +1,31 @@
-struct Bank {
-    
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    balance: i32,
+    holder: String
 }
 
-struct Account {
-    balance: i32,
-    id: u32,
-    holder: String
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account {
+            id,
+            balance: 0,
+            holder
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>
+}
+
+impl Bank {
+    fn new() -> Self {
+        Bank {
+            accounts: Vec::new()
+        }
+    }
 }
 
 fn main() {
