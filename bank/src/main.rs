1129849afea127c162e067c4fefd8b60994c2482
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-08-23 11:33:23 +0200

    Explain basics of ownership and borrowing

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -1,8 +1,14 @@
+// Ownership and borrowing in a nutshell:
+// Multiple things can refer to a value at the same time, but they are all read-only
+// A value can only be updated when there are no read-only references to it
+// This forms the basis of ownership and borrowing in Rust
+// The goal is to reduce the side effects of heap updates
+
 #[derive(Debug)]
 struct Account {
     id: u32,
     balance: i32,
-    holder: String
+    holder: String,
 }
 
 impl Account {
@@ -10,31 +16,32 @@ impl Account {
         Account {
             id,
             balance: 0,
-            holder
+            holder,
         }
     }
 }
 
 #[derive(Debug)]
 struct Bank {
-    accounts: Vec<Account>
+    accounts: Vec<Account>,
 }
 
 impl Bank {
     fn new() -> Self {
         Bank {
-            accounts: Vec::new()
+            accounts: Vec::new(),
         }
     }
 }
 
+fn print_account(account: Account) {
+    println!("{:#?}", account)
+}
+
 fn main() {
-    let bank = Bank::new();
-    let account = Account::new(
-        1,
-        String::from("me")
-    );
-    
-    println!("{:#?}", bank);
-    println!("{:#?}", account);
+    // let bank = Bank::new();
+    let account = Account::new(1, String::from("me"));
+
+    print_account(account);
+    print_account(account);
 }
